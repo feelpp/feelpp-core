5da45e2a2b5374f0140b9555b788cc9ccd432065
Vincent Chabannes <vincent.chabannes@feelpp.org>
2025-04-24 11:08:02 +0200

    add Environment class

--- src/feel/feelcore/environment.cpp
+++ src/feel/feelcore/environment.cpp
@@ -1,7 +1,100 @@
 //!
 #include <feel/feelcore/environment.hpp>
 
+#include <iostream>
+#include <fstream>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
 namespace Feel
 {
-  Environment::Environment(){}
+
+Environment::Environment( int argc, char* argv[], po::options_description const& optionsDescription )
+{
+    po::options_description fullOptionsDescription = this->options().add( optionsDescription );
+    po::store( po::command_line_parser( argc, argv ).options( fullOptionsDescription ).allow_unregistered().run(), M_vm );
+    po::notify( M_vm );
+
+    if ( M_vm.count( "help" ) )
+    {
+        std::cout << "Usage: options_description [options]\n";
+        std::cout << optionsDescription;
+        std::exit(0);
+    }
+
+    std::vector<std::string> configFiles;
+    if ( M_vm.count( "config-files" ) )
+    {
+        std::vector<std::string> configFilesOptVec = M_vm["config-files"].as<std::vector<std::string> >();
+        configFiles.insert( configFiles.end(), configFilesOptVec.begin(), configFilesOptVec.end() );
+    }
+
+    // reverse order (priorty for the last)
+    std::reverse(configFiles.begin(),configFiles.end());
+    for ( std::string const& cfgfile : configFiles )
+    {
+        if ( !fs::exists( cfgfile ) ) continue;
+        fs::path cfgAbsolutePath = fs::canonical( cfgfile );
+        log::info("Reading config-file {}",cfgAbsolutePath.string() );
+        std::ifstream ifs( cfgfile );
+        po::store( parse_config_file( ifs, fullOptionsDescription, true ), M_vm );
+    }
+    po::notify( M_vm );
 }
+
+Environment*
+Environment::instance()
+{
+    // TODO : assert if not init
+    return S_instance.get();
+}
+Environment*
+Environment::createInstance( int argc, char* argv[], po::options_description const& optionsDescription )
+{
+    S_instance = std::unique_ptr<Environment>{ new Environment( argc, argv, optionsDescription ) };
+    return S_instance.get();
+}
+
+
+po::options_description
+Environment::options() const
+{
+    po::options_description desc("Environment options");
+    desc.add_options()
+        ( "help", "produce help message" )
+        ( "verbose,v", po::value<int>()->implicit_value(1), "enable verbosity (optionally specify level)" )
+        ( "config-files", po::value<std::vector<std::string> >()->multitoken(), "specify a list of .cfg file" )
+        ;
+    return desc;
+
+}
+
+fs::path Environment::dataDir() const
+{
+    return fs::path( BOOST_PP_STRINGIZE(FEELPP_CORE_DATA_DIR) );
+}
+
+std::string
+Environment::expand( std::string const& expr ) const
+{
+    std::string res = expr;
+    std::vector<std::pair<std::string,std::string> > keyToValue = { std::make_pair( "${datadir}", this->dataDir().string() ) };
+    for ( auto const& [key,val] : keyToValue )
+        boost::ireplace_all( res, key, val );
+    return res;
+}
+
+
+std::unique_ptr<Environment> Environment::S_instance = nullptr;
+
+Environment* environment()
+{
+    return Environment::instance();
+}
+Environment* createEnvironment( int argc, char* argv[], po::options_description const& optionsDescription )
+{
+    return Environment::createInstance( argc, argv, optionsDescription );
+}
+
+}// namespace Feel::Ktirio::Geom
+
--- src/feel/feelcore/environment.hpp
+++ src/feel/feelcore/environment.hpp
@@ -15,30 +15,30 @@
 #include <feel/feelcore/namedarguments.hpp>
 
 #if defined _WIN32 || defined __CYGWIN__
-  #if defined(FEELPP_CORE_LIB_LIBRARY_EXPORTING)
-    #ifdef __GNUC__
-      #define FEELPP_DECL_EXPORT __attribute__ ((dllexport))
-    #else
-      #define FEELPP_DECL_EXPORT __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
-    #endif
-  #else
-    #ifdef __GNUC__
-      #define FEELPP_DECL_IMPORT __attribute__ ((dllimport))
-    #else
-      #define FEELPP_DECL_IMPORT __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
-    #endif
-  #endif
-  #define FEELPP_DECL_HIDDEN
+#if defined(FEELPP_CORE_LIB_LIBRARY_EXPORTING)
+#ifdef __GNUC__
+#define FEELPP_DECL_EXPORT __attribute__ ((dllexport))
 #else
-  #if __GNUC__ >= 4 || defined(__clang__)
-    #define FEELPP_DECL_EXPORT     __attribute__((visibility("default")))
-    #define FEELPP_DECL_IMPORT     __attribute__((visibility("default")))
-    #define FEELPP_DECL_HIDDEN     __attribute__((visibility("hidden")))
-  #else
-    #define FEELPP_DECL_EXPORT
-    #define FEELPP_DECL_IMPORT
-    #define FEELPP_DECL_HIDDEN
-  #endif
+#define FEELPP_DECL_EXPORT __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+#endif
+#else
+#ifdef __GNUC__
+#define FEELPP_DECL_IMPORT __attribute__ ((dllimport))
+#else
+#define FEELPP_DECL_IMPORT __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+#endif
+#endif
+#define FEELPP_DECL_HIDDEN
+#else
+#if __GNUC__ >= 4 || defined(__clang__)
+#define FEELPP_DECL_EXPORT     __attribute__((visibility("default")))
+#define FEELPP_DECL_IMPORT     __attribute__((visibility("default")))
+#define FEELPP_DECL_HIDDEN     __attribute__((visibility("hidden")))
+#else
+#define FEELPP_DECL_EXPORT
+#define FEELPP_DECL_IMPORT
+#define FEELPP_DECL_HIDDEN
+#endif
 #endif
 
 #if defined(FEELPP_CORE_LIB_LIBRARY_EXPORTING)
@@ -51,10 +51,82 @@
 namespace Feel
 {
 
-  //! class build as singleton that represent environment
-  class FEELPP_CORE_EXPORT Environment
-    {
-    public:
-    Environment();
-    };
+namespace fs = std::filesystem;
+namespace po = boost::program_options;
+namespace log = spdlog;
+namespace nl = nlohmann;
+//using json = nl::json;
+
+//! class build as singleton that represent environment
+class FEELPP_CORE_EXPORT Environment
+{
+    //! support singleton with only private constuctor.
+    Environment( int argc, char* argv[], po::options_description const& optionsDescription = {} );
+  public :
+  //! singletons should not be cloneable.
+  Environment(Environment &other) = delete;
+  //! singletons should not be assignable.
+  void operator=(const Environment &) = delete;
+
+  po::variables_map const& vm() const { return M_vm; }
+  //! return option description of environment
+  po::options_description options() const;
+
+  //! return data directory (Warning currently defined on sources directory)
+  fs::path dataDir() const;
+
+  //! return string by expanding some keyword of format ${key}
+  std::string expand( std::string const& expr ) const;
+
+  private:
+  //! return singleton instance
+  static Environment* instance();
+  static Environment* createInstance( int argc, char* argv[], po::options_description const& optionsDescription = {} );
+
+  friend FEELPP_CORE_EXPORT Environment* createEnvironment( int argc, char* argv[], po::options_description const& optionsDescription );
+  friend FEELPP_CORE_EXPORT Environment* environment();
+  private:
+  static std::unique_ptr<Environment> S_instance;
+  po::variables_map M_vm;
+};
+
+FEELPP_CORE_EXPORT Environment* createEnvironment( int argc, char* argv[], po::options_description const& optionsDescription = {} );
+
+FEELPP_CORE_EXPORT Environment* environment();
+
+
+
+template <typename ... Ts>
+auto optionIterator( Ts && ... v )
+{
+    auto args = NA::make_arguments( std::forward<Ts>(v)... );
+    std::string const& name  = args.get(_name);
+    std::string const& prefix = args.get_else(_prefix, "" );
+    po::variables_map const& vm = args.get_else(_vm, environment()->vm() );
+    auto opt = fmt::memory_buffer();
+    if ( !prefix.empty() )
+        fmt::format_to( std::back_inserter(opt), "{}.",prefix);
+    fmt::format_to( std::back_inserter( opt ), "{}",name);
+    std::string optname = fmt::to_string(opt);
+    auto it = vm.find( optname );
+    return std::make_tuple( it != vm.end(), it, std::move( optname ) );
+}
+
+template <typename ... Ts>
+bool hasOption( Ts && ... v )
+{
+    auto args = NA::make_arguments( std::forward<Ts>(v)... );
+    return std::get<0>( optionIterator( std::forward<Ts>(v)... ) );
+}
+
+template <typename ... Ts>
+po::variable_value option( Ts && ... v )
+{
+    auto args = NA::make_arguments( std::forward<Ts>(v)... );
+    po::variables_map const& vm = args.get_else(_vm, environment()->vm() );
+    auto [exist,it,optname] = optionIterator( std::forward<Ts>(v)... );
+    ASSERT( exist, fmt::format( "invalid or missing option {}",optname ) );
+    return it->second;
+}
+
 }
