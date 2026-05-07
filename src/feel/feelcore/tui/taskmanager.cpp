bef4b62b398af552ce60e74b62ef78d43d400e7e
Javier Cladellas <javiercladellas@gmail.com>
2026-05-07 14:46:15 +0200

    add tui lib and init AsyncUiTask

---
+++ src/feel/feelcore/tui/taskmanager.cpp
@@ -0,0 +1,69 @@
+//!
+
+
+#include <fmt/format.h>
+
+#include <feel/feelcore/tui/taskmanager.hpp>
+
+
+
+namespace Feel::Core::ftxui
+{
+
+
+void
+AsyncUiTask::executeTask()
+{
+    std::string internalResult;
+    TaskStatus internalStatus = TaskStatus::WORKING; 
+    M_state.status = internalStatus;
+
+    try
+    {
+        internalResult = M_task();
+        internalStatus = TaskStatus::SUCCESS;
+    }
+    catch (std::exception const& e )
+    {
+        internalResult = fmt::format( "Error: {}", e.what() );
+        internalStatus = TaskStatus::ERROR; 
+    }
+
+    M_screen.Post([this, internalResult, internalStatus]()
+    {
+        M_state.status = internalStatus;
+        M_state.result = internalResult;
+    });
+    M_screen.PostEvent( ::ftxui::Event::Custom );
+}
+
+void
+AsyncUiTask::pumpTicks()
+{
+    while (M_state.status == TaskStatus::WORKING)
+    {
+        std::this_thread::sleep_for(M_tickDuration);
+        M_state.loadingFrameCount++;
+        M_screen.PostEvent( ::ftxui::Event::Custom );
+    }
+}
+
+void
+AsyncUiTask::start()
+{
+    // prevents double trigger
+    if (M_state.status == TaskStatus::WORKING) return;
+
+    std::thread( [this](){ this->executeTask(); }).detach();
+    std::thread( [this](){ this->pumpTicks(); }).detach();
+}
+
+void
+AsyncUiTask::reset()
+{
+    M_state.status = TaskStatus::IDLE;
+    M_state.result = "";
+    M_state.loadingFrameCount = 0;
+}
+
+} // namespace Feel::Core::ftxui
---
+++ src/feel/feelcore/tui/taskmanager.hpp
@@ -0,0 +1,65 @@
+//!
+
+#pragma once
+
+#include <atomic>
+#include <chrono>
+
+#include "ftxui/component/screen_interactive.hpp"
+
+
+
+namespace Feel::Core::ftxui
+{
+
+//! Current execution lifecycle of the async task.
+enum class TaskStatus { IDLE, WORKING, SUCCESS, ERROR };
+
+//! Thread safe state of the async task
+struct AsyncUiTaskState
+{
+    std::string result;
+    std::atomic<TaskStatus> status {TaskStatus::IDLE};
+    std::atomic<std::size_t> loadingFrameCount = {0};
+};
+
+//! Manages a background task while keeping an FTXUI screen responsive 
+//! Updates the screen each tick (allows load animations)
+class AsyncUiTask
+{
+public: 
+
+    //! task: Background task to execute, should return either a success message if OK and can raise an exception on error
+    //! screen: An FTXUI screen to post UI refresh events to
+    //! tickDuration: The interval between forced UI redraws while the task is running
+    AsyncUiTask( std::function<std::string()> task,
+                 ::ftxui::ScreenInteractive & screen, 
+                 std::chrono::milliseconds tickDuration = std::chrono::milliseconds(200) )
+        : M_task(std::move(task)), M_screen(screen), M_tickDuration(tickDuration)
+    {}
+
+    //! Spawns a worker and tick threads.
+    void start();
+
+    //! Clears the task state and goes back to IDLE .
+    void reset();
+
+    //! Retrieves the current task state.
+    AsyncUiTaskState const& getState() const { return M_state; };
+
+private:
+    //! Executes a task and posts the result to the UI
+    void executeTask();
+
+    //! Sends a UI event each tick milliseconds to redraw
+    void pumpTicks();
+
+private:
+    std::function<std::string()> M_task;
+    ::ftxui::ScreenInteractive & M_screen;
+    AsyncUiTaskState M_state;
+    std::chrono::milliseconds M_tickDuration;
+};
+
+
+}
