8dbfafc94dfad5fc14915dc0e2d728bc427a4421
Javier Cladellas <javiercladellas@gmail.com>
2026-05-07 15:37:51 +0200

    add slider and spinbox

--- src/feel/feelcore/tui/components.cpp
+++ src/feel/feelcore/tui/components.cpp
@@ -17,10 +17,11 @@ MultiOptionSelector( std::vector<std::pair<std::string,bool*>> const& options, s
         checkboxes->Add( Checkbox( opt.first, opt.second ) );
 
     return Renderer( checkboxes, [label,checkboxes] {
-        return window( text( fmt::format( " {} ", label ) ) | bold | center, 
+        return window(
+            text( fmt::format( " {} ", label ) ) | bold | center, 
             vbox( { checkboxes->Render() } )
         );
-    });
+    } );
 }
 
 Component
@@ -29,12 +30,53 @@ RadioSelector( std::vector<std::string> const* entries, int * selected, std::str
     Component radiobox = Radiobox( entries, selected );
 
     return Renderer( radiobox, [label, radiobox] {
-        return window( text( fmt::format( " {} ", label ) ) | bold | center,
+        return window(
+            text( fmt::format( " {} ", label ) ) | bold | center,
             vbox( { radiobox->Render() })
         );
-    });
+    } );
 }
 
 
+template <typename T>
+Component
+ReadoutSlider( Ref<T> value, T min, T max, T step, std::string const& title, int displayPrecision )
+{
+    Component slider = Slider( "", value, min, max, step );
+
+    return Renderer( slider, [slider, value, title, displayPrecision] {
+        return vbox( {
+            text( title ) | dim,
+            hbox( {
+                slider->Render() | flex,
+                text( fmt::format("{:>3.{}f}", static_cast<float>( *value ), displayPrecision ) ) | size( WIDTH, EQUAL, 6 ) 
+            } ),
+            separatorEmpty()
+        } );
+    } );
+}
+template Component ReadoutSlider<float>( Ref<float>, float, float, float, const std::string&, int );
+template Component ReadoutSlider<int>( Ref<int>, int, int, int, const std::string&, int );
+
+
+
+Component
+SpinBox( int & value, std::string const& title )
+{
+    Component inputMinus = Button( "-", [&value]{ value--; }, ButtonOption::Ascii() );
+    Component inputPlus = Button( "+", [&value]{ value++; }, ButtonOption::Ascii() );
+    Component stepperContainer = Container::Horizontal( { inputMinus, inputPlus } );
+
+    return Renderer( stepperContainer, [&value, inputMinus, inputPlus, title] {
+        return hbox( {
+            text( title ),
+            hbox( {
+                inputMinus->Render(),
+                text( std::to_string( value ) ) | center,
+                inputPlus->Render(),
+            } ) | center,
+        } );
+    } );
+}
 
 } //namespace Feel::Core::ftxui
--- src/feel/feelcore/tui/components.hpp
+++ src/feel/feelcore/tui/components.hpp
@@ -16,5 +16,11 @@ Component MultiOptionSelector( std::vector<std::pair<std::string,bool*>> const& 
 //! Styled and labeled window with vertical radio buttons
 Component RadioSelector( std::vector<std::string> const* entries, int * selected, std::string const& label = "" );
 
+template <typename T>
+Component ReadoutSlider( Ref<T> value, T min = 0., T max = 1., T step = 0.1,
+                         std::string const& title = "", int displayPrecision = 1 );
+
+Component SpinBox( int & value, std::string const& title = "" );
+
 
 } //namespace Feel::Core::ftxui
