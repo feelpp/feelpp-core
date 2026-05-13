191c2cc66fb71691355ba59e8db04f468d7a2449
Javier Cladellas <javiercladellas@gmail.com>
2026-05-13 17:25:37 +0200

    add some ftxui testing

--- tests/test-ftxui.cpp
+++ tests/test-ftxui.cpp
@@ -1,7 +1,7 @@
 //!
 
 #include <ftxui/component/component.hpp>
-#include <iostream>
+#include <ftxui/component/screen_interactive.hpp>
 
 #include <catch2/catch_session.hpp>
 #include <catch2/catch_test_macros.hpp>
@@ -17,46 +17,7 @@
 
 #include <feel/feelcore/environment.hpp>
 #include <stdexcept>
-
-TEST_CASE( "Test Screen Basic", "[FTXUI]" )
-{
-    using namespace ftxui;
-    Element document = hbox({
-            text("left")   | border,
-            text("middle") | border | flex,
-            text("right")  | border,
-        });
-    auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-    Render(screen, document);
-    screen.Print();
-}
-
-TEST_CASE( "Test Gauge", "[FTXUI]" )
-{
-    using namespace ftxui;
-    using namespace std::chrono_literals;
-
-    std::string reset_position;
-    for (float percentage = 0.0f; percentage <= 1.0f; percentage += 0.01f) {
-        std::string data_downloaded =
-            std::to_string(int(percentage * 5000)) + "/5000";
-        auto document = hbox({
-                text("downloading:"),
-                gauge(percentage) | flex,
-                text(" " + data_downloaded),
-            });
-        auto screen = Screen(100, 1);
-        Render(screen, document);
-        std::cout << reset_position;
-        screen.Print();
-        reset_position = screen.ResetPosition();
-
-        std::this_thread::sleep_for(0.01s);
-    }
-    std::cout << std::endl;
-}
-
-
+#include <thread>
 
 
 
@@ -71,13 +32,36 @@ TEST_CASE( "Test MultiOptionSelector", "[FEELCORE-TUI]" )
         { "option1", &option1 }, { "option2", &option2 }, { "option3", &option3 }
     };
 
-    Component multiOptionSelector = MultiOptionSelector( options, "MultiOption Selector" );
+    std::string title = "MultiOption Selector";
+    Component multiOptionSelector = MultiOptionSelector( options, title );
 
     auto document = multiOptionSelector->Render();
 
     auto screen = Screen::Create( Dimension::Full(), Dimension::Fit( document ) );
     Render( screen, document );
-    screen.Print();
+
+    std::string screenContent = screen.ToString();
+
+    REQUIRE( screenContent.contains( title ) );
+    for ( auto & [label, v] : options )
+        REQUIRE( screenContent.contains( label ) );
+
+
+    //Cursor should already be in option one
+    bool evtHandled = multiOptionSelector->OnEvent( Event::Return );
+    REQUIRE( evtHandled );
+    REQUIRE( option1 == false );
+    REQUIRE( option2 == false ); //unchanged
+    REQUIRE( option3 == true ); //unchanged
+
+
+    //Trigger second option
+    multiOptionSelector->OnEvent( Event::ArrowDown );
+    multiOptionSelector->OnEvent( Event::Return );
+    REQUIRE( option1 == false );
+    REQUIRE( option2 == true );
+    REQUIRE( option3 == true ); //unchanged
+
 }
 
 
@@ -88,13 +72,31 @@ TEST_CASE( "Test RadioSelector", "[FEELCORE-TUI]" )
     int selected = 0;
     std::vector<std::string> options = { "option1", "option2", "option3" };
 
-    Component radioSelector = RadioSelector( &options, &selected, "Radio Selector" );
+    std::string title = "Radio Selector";
+    Component radioSelector = RadioSelector( &options, &selected, title );
 
     auto document = radioSelector->Render();
 
     auto screen = Screen::Create( Dimension::Full(), Dimension::Fit( document ) );
     Render( screen, document );
-    screen.Print();
+
+    std::string screenContent = screen.ToString();
+
+    REQUIRE( screenContent.contains( title ) );
+    for ( auto & label : options )
+        REQUIRE( screenContent.contains( label ) );
+
+
+    bool evtHandled = radioSelector->OnEvent( Event::Return );
+    REQUIRE( evtHandled );
+    REQUIRE( selected == 0 );
+
+
+    //Trigger last option (down two times)
+    radioSelector->OnEvent( Event::ArrowDown );
+    radioSelector->OnEvent( Event::ArrowDown );
+    radioSelector->OnEvent( Event::Return );
+    REQUIRE( selected == 2 );
 }
 
 
@@ -103,10 +105,12 @@ TEST_CASE( "Test ReadoutSlider", "[FEELCORE-TUI]" )
     using namespace Feel::Core::ftxui;
 
     int intValue = 50;
-    Component intSlider = ReadoutSlider<int>( &intValue, 0, 100, 1, "Int Readout Slider", 0 ); 
+    std::string intTitle = "Int Readout Slider";
+    Component intSlider = ReadoutSlider<int>( &intValue, 0, 100, 1, intTitle, 0 ); 
 
     float doubleValue = 0.5;
-    Component doubleSlider = ReadoutSlider<float>( &doubleValue, 0, 1, 0.1, "Double Readout Slider", 2 ); 
+    std::string doubleTitle = "Double Readout Slider";
+    Component doubleSlider = ReadoutSlider<float>( &doubleValue, 0, 1, 0.1, doubleTitle, 2 ); 
 
     auto document = hbox( {
         intSlider->Render() | flex_grow,
@@ -115,7 +119,26 @@ TEST_CASE( "Test ReadoutSlider", "[FEELCORE-TUI]" )
 
     auto screen = Screen::Create( Dimension::Full(), Dimension::Fit( document ) );
     Render( screen, document );
-    screen.Print();
+
+    std::string screenContent = screen.ToString();
+
+    REQUIRE( screenContent.contains( intTitle ) );
+    REQUIRE( screenContent.contains( doubleTitle ) );
+
+    //Check that initial values appear on screen
+    REQUIRE( screenContent.contains( "50" ) );
+    REQUIRE( screenContent.contains( "0.50" ) ); //Because display precision of 2
+
+    bool evtHandled = intSlider->OnEvent( Event::ArrowRight );
+    REQUIRE( evtHandled );
+    REQUIRE( intValue == 50 + 1 );
+    intSlider->OnEvent( Event::ArrowLeft );
+    REQUIRE( intValue == 50 );
+
+    doubleSlider->OnEvent( Event::ArrowRight );
+    REQUIRE( doubleValue == Catch::Approx( 0.5 + 0.1) );
+    doubleSlider->OnEvent( Event::ArrowLeft );
+    REQUIRE( doubleValue == Catch::Approx( 0.5 ) );
 }
 
 
@@ -123,13 +146,26 @@ TEST_CASE( "Test SpinBox", "[FEELCORE-TUI]" )
 {
     using namespace Feel::Core::ftxui;
 
-    int value = 0;
+    int value = 1;
     Component spinBox = SpinBox( value, "SpinBox " ); 
 
     auto document = spinBox->Render();
     auto screen = Screen::Create( Dimension::Full(), Dimension::Fit( document ) );
     Render( screen, document );
-    screen.Print();
+
+    auto screenContent = screen.ToString();
+
+    REQUIRE( screenContent.contains("+") );
+    REQUIRE( screenContent.contains("-") );
+
+
+    bool evtHandled = spinBox->OnEvent( Event::Return ); //cursor already on -, press it
+    REQUIRE( evtHandled );
+    REQUIRE( value == 0 );
+    spinBox->OnEvent( Event::ArrowRight ); //Position on the + elt
+    spinBox->OnEvent( Event::Return ); //Press on the + button
+    spinBox->OnEvent( Event::Return ); //Press on the + button
+    REQUIRE( value == 2 );
 }
 
 
@@ -138,13 +174,28 @@ TEST_CASE( "Test FileInput", "[FEELCORE-TUI]" )
     using namespace Feel::Core::ftxui;
 
     std::string filepath;
-    Component fileInput = FileInput( filepath, " Enter your filepath..." ); 
+    std::string placeholder = " Enter your filepath...";
+    Component fileInput = FileInput( &filepath, placeholder ); 
 
     auto document = fileInput->Render();
     auto screen = Screen::Create( Dimension::Full(), Dimension::Fit( document ) );
     Render( screen, document );
-    screen.Print();
 
+    std::string screenContent = screen.ToString();
+
+    REQUIRE( screenContent.contains( placeholder ) );
+
+    bool evtHandled = fileInput->OnEvent( Event::Character("./") );
+    REQUIRE( evtHandled );
+
+    //Check cycling
+    fileInput->OnEvent( Event::Tab );
+    REQUIRE( filepath != "./" ); 
+    REQUIRE( Feel::fs::exists( filepath ) );
+    std::string oldPath = filepath;
+    fileInput->OnEvent( Event::Tab );
+    REQUIRE(oldPath != filepath );
+    REQUIRE( Feel::fs::exists( filepath ) );
 
 }
 
@@ -152,7 +203,6 @@ TEST_CASE( "Test FileInput", "[FEELCORE-TUI]" )
 TEST_CASE( "Test TaskManager Success", "[FEELCORE-TUI]" )
 {
     using namespace Feel::Core::ftxui;
-    using namespace std::chrono_literals;
 
     auto screen = ScreenInteractive::TerminalOutput();
 
@@ -163,8 +213,7 @@ TEST_CASE( "Test TaskManager Success", "[FEELCORE-TUI]" )
     screen.Loop( Renderer([&screen, &task] {
         if ( task.getState().status == TaskStatus::IDLE )
             task.start();
-
-        if ( task.getState().status != TaskStatus::WORKING )
+        else if ( task.getState().status != TaskStatus::WORKING )
             screen.Post( screen.ExitLoopClosure() );
         return text( "Testing async UI task ..." );
     } ) );
@@ -178,7 +227,6 @@ TEST_CASE( "Test TaskManager Success", "[FEELCORE-TUI]" )
 TEST_CASE( "Test TaskManager Failure", "[FEELCORE-TUI]" )
 {
     using namespace Feel::Core::ftxui;
-    using namespace std::chrono_literals;
 
     auto screen = ScreenInteractive::TerminalOutput();
 
@@ -189,8 +237,7 @@ TEST_CASE( "Test TaskManager Failure", "[FEELCORE-TUI]" )
     screen.Loop( Renderer([&screen, &task] {
         if ( task.getState().status == TaskStatus::IDLE )
             task.start();
-
-        if ( task.getState().status != TaskStatus::WORKING )
+        else if ( task.getState().status != TaskStatus::WORKING )
             screen.Post( screen.ExitLoopClosure() );
         return text( "Testing async UI task ..." );
     } ) );
@@ -203,6 +250,55 @@ TEST_CASE( "Test TaskManager Failure", "[FEELCORE-TUI]" )
 
 TEST_CASE( "Test WorkerButton", "[FEELCORE-TUI]" )
 {
+#if 0
+    using namespace Feel::Core::ftxui;
+    using namespace std::chrono_literals;
+
+
+    auto screen = ScreenInteractive::FitComponent();
+    auto successTask = [] -> std::string
+    { 
+        std::this_thread::sleep_for( 1s );
+        return "Success!"; 
+    };
+   
+    std::string btnTitle = "Click";
+    Component successButton = WorkerButton( screen, successTask, btnTitle );
+
+    std::string initialState;
+    std::string loadingState;
+    std::string successState;
+
+    std::thread testSequence([&] {
+        std::this_thread::sleep_for( 100ms );
+
+        screen.Post( [&] {
+            initialState = screen.ToString();
+            screen.PostEvent( Event::Return );
+        });
+
+        std::this_thread::sleep_for( 200ms );
+        screen.Post( [&] { loadingState = screen.ToString(); });
+
+        std::this_thread::sleep_for( 1200ms );
+        screen.Post( [&] {
+            successState = screen.ToString();
+            screen.ExitLoopClosure()();
+        });
+    });
+
+    screen.Loop( Renderer(successButton, [&successButton]{ return successButton->Render(); } ) );
+
+    if (testSequence.joinable())
+        testSequence.join();
+ 
+    REQUIRE( initialState.contains(btnTitle) );
+    REQUIRE( loadingState.contains("Loading") );
+    REQUIRE( successState.contains("Success!") );
+
+    std::this_thread::sleep_for( 1200ms );
+
+#endif
 }
 
 
