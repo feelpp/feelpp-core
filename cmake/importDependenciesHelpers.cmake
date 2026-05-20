b28a2ed4e0bcc04fd18c589920f0527789d7327e
Javier Cladellas <javiercladellas@gmail.com>
2026-05-20 17:04:21 +0200

    add optional tbb dep

--- cmake/importDependenciesHelpers.cmake
+++ cmake/importDependenciesHelpers.cmake
@@ -382,9 +382,7 @@ macro(importDependency_CGAL _useSystem _target_dependencies _target_definitions 
 
     set(CGAL_TARGET_LIST "CGAL::CGAL;CGAL::CGAL_Core;CGAL::Eigen3_support")
 
-    find_package(TBB QUIET)
-    if (TBB_FOUND)
-        message(STATUS "CGAL: System TBB found" )
+    if ( FEELPP_HAS_TBB )
         include(CGAL_TBB_support)
         string(APPEND CGAL_TARGET_LIST ";CGAL::TBB_support")
     endif()
@@ -449,6 +447,27 @@ macro(importDependency_PNG _useSystem _target_dependencies _target_definitions _
   endif()
   printDependencySectionEnd("PNG")
 endmacro(importDependency_PNG)
+
+#--------------------------------------
+# TBB
+#--------------------------------------
+macro(importDependency_TBB _useSystem _target_dependencies _target_definitions _cmakeVariablePrefix)
+  printDependencySectionBegin("TBB")
+  if ( ${_useSystem} )
+      find_package(TBB QUIET)
+  else()
+    message(FATAL_ERROR "TBB dependency can be enabled only from system")
+  endif()
+  if ( TARGET TBB::tbb  )
+    message(STATUS "TBB found")
+    feelpp_updateImportDependencyForUse( TBB TBB::tbb ${_useSystem} ${_target_dependencies} ${_target_definitions} ${_cmakeVariablePrefix} )
+  else()
+    message(STATUS "TBB not found -> disabling TBB support")
+    set(FEELPP_ENABLE_TBB OFF CACHE BOOL "" FORCE)
+  endif()
+  printDependencySectionEnd("TBB")
+endmacro(importDependency_TBB)
+
 
 #--------------------------------------
 # Catch2
