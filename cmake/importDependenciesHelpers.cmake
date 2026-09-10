28d8113ee0dcf64d9d5e13cb68a98de3dc17d5e4
Javier Cladellas <82624449+JavierCladellas@users.noreply.github.com>
2026-09-10 15:40:07 +0200

    Update CGAL version to v6.2.1

--- cmake/importDependenciesHelpers.cmake
+++ cmake/importDependenciesHelpers.cmake
@@ -351,7 +351,7 @@ macro(importDependency_CGAL _useSystem _target_dependencies _target_definitions 
     find_package(CGAL REQUIRED COMPONENTS Core)
   else()
     FetchContent_Declare( cgal GIT_REPOSITORY https://github.com/CGAL/cgal.git
-      GIT_TAG v6.2
+      GIT_TAG v6.2.1
       GIT_SHALLOW ON
     )
     set( WITH_CGAL_Qt5 OFF )
